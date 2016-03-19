15c8e942e941333191b4994e5bee52a278c5a423
5mil <jdaurio09@gmail.com>
2016-03-19 17:06:33 -0400

    stuff

--- src/main.h
+++ src/main.h
@@ -35,14 +35,14 @@ static const unsigned int MAX_ORPHAN_TRANSACTIONS = MAX_BLOCK_SIZE/100;
 static const int64 MIN_TX_FEE = CENT;
 static const int64 MIN_RELAY_TX_FEE = CENT;
 static const int64 MAX_MONEY = 200000000 * COIN;
-static const int64 MAX_MINT_PROOF_OF_WORK = 999 * COIN;
+static const int64 MAX_MINT_PROOF_OF_WORK = 1 * COIN;
 static const int64 MIN_TXOUT_AMOUNT = MIN_TX_FEE;
 inline bool MoneyRange(int64 nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 static const int COINBASE_MATURITY_BALLS = 50;
 // Threshold for nLockTime: below this value it is interpreted as block number, otherwise as UNIX timestamp.
 static const int LOCKTIME_THRESHOLD = 500000000; // Tue Nov  5 00:53:20 1985 UTC
 static const int STAKE_TARGET_SPACING = 2 * 60; // 2-minute block spacing 
-static const int STAKE_MIN_AGE = 60 * 60 * 12 * 1; // minimum age for coin age 12 hours
+static const int STAKE_MIN_AGE = 60 * 60 * 6 * 1; // minimum age for coin age 12 hours
 static const int STAKE_MAX_AGE = 60 * 60 * 24 * 1; // stake age of full weight 24 hours
 
 #ifdef USE_UPNP
